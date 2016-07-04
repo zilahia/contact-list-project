203c81547eef14efca5e92bf848b94a5e154e0a4
David Zilahi <david.zilahi@ge.com>
2016-07-04 10:25:40 +0200

    initiating new build

--- newbuild.sh
+++ newbuild.sh
@@ -2,6 +2,11 @@
 
 echo "build" >> builds.txt
 
+git submodule foreach git clean -xfd
+git submodule foreach git reset --hard
+git submodule foreach git checkout master
+git submodule foreach git pull origin master
+
 git submodule update --remote
 git commit -a -m "initiating new build"
 git push origin master
